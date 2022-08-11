8a8dcab5cd217edf62e8531017f29f14a527ec62
William Cadiz <wcadz27@gmail.com>
2022-08-11 15:51:15 -0700

    Tweaked some sizing issues on the work container

--- portfolio-app/src/container/Work/Work.jsx
+++ portfolio-app/src/container/Work/Work.jsx
@@ -1,8 +1,11 @@
 import { motion } from "framer-motion";
 import React from "react";
+import LazyLoad from "react-lazyload";
 import { AiOutlineGithub, AiFillEye } from "react-icons/ai";
 
 import "./Work.scss";
+import MovieAppImage from "../../assets/movie-app-image.png";
+import PortfolioImage from "../../assets/portfolio-image.png";
 
 const Projects = {
   data: [
@@ -14,7 +17,7 @@ const Projects = {
           links: [
             { website: "hello.com" },
             { github: "github.hello" },
-            { imgURL: "https://imgur.com/gXWNWXS" },
+            { imgURL: MovieAppImage },
           ],
         },
         {
@@ -24,11 +27,20 @@ const Projects = {
       ],
     },
     {
-      name: "Sample2",
+      name: "My Portfolio",
       className: "sample-two project-container",
       details: [
-        { links: [{ website: "hello" }, { github: "hello" }, { imgURL: "" }] },
-        { description: "this is another sample website" },
+        {
+          links: [
+            { website: "hello" },
+            { github: "hello" },
+            { imgURL: PortfolioImage },
+          ],
+        },
+        {
+          description:
+            "A simple front end portfolio app built with React, SASS, and tsParticles as the background",
+        },
       ],
     },
   ],
@@ -50,19 +62,21 @@ const Work = ({ theme }) => {
       </motion.div>
       <motion.div className="app__work-projects-container">
         {Projects.data.map((item, index) => (
-          <div key={index} className={item.className}>
-            <div className="img-container">
-              {/* <img alt="" className={}/> */}
-              <div className="work-links-container">
-                <motion.div
-                  className="app__work-hover"
-                  whileHover={{ opacity: [0, 1] }}
-                  transition={{
-                    duration: 0.25,
-                    ease: "easeInOut",
-                    staggerChildren: 0.5,
-                  }}
-                >
+          <div key={index} className={`${item.className} work-container`}>
+            <div className="work-links-container">
+              <LazyLoad className="img-container">
+                <img src={item.details[0].links[2].imgURL} alt={item.name} />
+              </LazyLoad>
+              <motion.div
+                className="app__work-hover"
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+              >
+                <div className="icons-container">
                   <motion.div
                     className="icon-container"
                     whileInView={{ scale: [0, 1] }}
@@ -83,12 +97,12 @@ const Work = ({ theme }) => {
                       <AiFillEye className="work-link-icon" />
                     </a>
                   </motion.div>
-                </motion.div>
-              </div>
-            </div>
-            <div className="project-info">
-              <h3 className="p-text">{item.name}</h3>
-              <p className="p-text">{item.details[1].description}</p>
+                </div>
+                <div className="project-info">
+                  <h3 className="p-text">{item.name}</h3>
+                  <p className="p-text">{item.details[1].description}</p>
+                </div>
+              </motion.div>
             </div>
           </div>
         ))}
